0b4ddc9fda4b83a4198bf65dd04f86e5af27dafa
jenicaliang <112435176+jenicaliang@users.noreply.github.com>
2026-04-21 14:34:30 -0500

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import Desktop from './pages/Desktop'
 import BootScreen from './components/desktop/BootScreen'
 import MobileGate from "./components/MobileGate";
